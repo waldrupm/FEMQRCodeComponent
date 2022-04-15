fb260626d4cf7b599af73a17ab54f9cd5094183a
Micheal Waldrup <waldrupm@gmail.com>
2022-04-15 11:37:07 -0400

    configure tailwind, build and style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    colors: {
+      "bright-blue": "#2C7DFA",
+      "blue-shade": "#3685FF",
+      "dark-navy": "#1F314F",
+      "med-gray": "#7D889E",
+      "lgt-gray": "#D5E1EF",
+      white: "#FFFFFF",
+    },
+    fontSize: {
+      "1xl": "1.40rem",
+      sm: "0.935rem",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Outfit", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
